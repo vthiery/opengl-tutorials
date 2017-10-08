cb98cddf1999315fb783e1baf2412fb0a98adece
Vincent Thiery <vjmthiery@gmail.com>
2017-10-08 20:33:20 +0200

    Make the cube color like crazy

--- tutorial04_colored_cube/playground.cpp
+++ tutorial04_colored_cube/playground.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <random>
 #include <GL/glew.h>
 #include <glfw3.h>
 
@@ -118,46 +119,6 @@ int main()
 		1.0f,-1.0f, 1.0f
 	};
 
-	// One color for each vertex. They were generated randomly.
-	static const GLfloat g_color_buffer_data[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
-	};
-
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -166,8 +127,18 @@ int main()
 	GLuint colorbuffer;
 	glGenBuffers(1, &colorbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	GLfloat g_color_buffer_data[12 * 3 * 3];
 
+	std::srand(123);
+	auto colorIt = [&g_color_buffer_data]() {
+		// Generate the colors randomly
+		for (size_t v = 0; v < 12 * 3; ++v) {
+			g_color_buffer_data[3 * v + 0] = static_cast<float>(std::rand()) / RAND_MAX;
+			g_color_buffer_data[3 * v + 1] = static_cast<float>(std::rand()) / RAND_MAX;
+			g_color_buffer_data[3 * v + 2] = static_cast<float>(std::rand()) / RAND_MAX;
+		}
+	};
+	
 	do {
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -189,7 +160,9 @@ int main()
 
 		// 2nd attribute buffer : colors
 		glEnableVertexAttribArray(1);
+		colorIt();
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 		glVertexAttribPointer(
 			1,                  // attribute. No particular reason for 1, but must match the layout in the shader.
 			3,                  // size
